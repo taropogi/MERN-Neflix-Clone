4643e77d379bd2c864fb9b178bb68a84125dd1f4
Richard Bernisca <taropogi_123@yahoo.com>
2024-07-24 01:16:45 +0800

    auth routes

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 const app = express();
 
-app.get("/signup", (req, res) => {
-  res.send("signup");
-});
+//routes
+import authRoutes from "./routes/auth.route.js";
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server running...");

ab882e32484f5facc98ef22aaf61498d58af7529
Richard Bernisca <taropogi_123@yahoo.com>
2024-08-17 01:50:55 +0800

    redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,25 @@ import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import AppLayout from "./UI/AppLayout";
 import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <>
       <Routes>
         <Route element={<AppLayout />}>
           <Route index element={<Navigate replace to="home" />} />
           <Route path="home" element={<HomePage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignupPage />} />
+          <Route
+            path="login"
+            element={!user ? <LoginPage /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path="signup"
+            element={!user ? <SignupPage /> : <Navigate to={"/"} />}
+          />
         </Route>
       </Routes>
       <Toaster />
